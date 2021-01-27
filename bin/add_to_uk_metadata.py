dcb954a4ffc632da24ea0e478dd5772c7bed2a3b
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2021-01-27 20:38:19 +0000

    update with bens preprocessing work

--- bin/add_to_uk_metadata.py
+++ bin/add_to_uk_metadata.py
@@ -1,97 +1,146 @@
 #!/usr/bin/env python3
 
-import pandas as pd
 import sys
 import argparse
+import csv
+from itertools import chain
+from epiweeks import Week,Year
+from datetime import datetime
+
+adm1a_to_country = {"UK-SCT": "Scotland",
+                    "UK-WLS": "Wales",
+                    "UK-ENG": "England",
+                    "UK-NIR": "Northern_Ireland"}
 
 def parse_args():
     parser = argparse.ArgumentParser(description="""Add sample_date, pillar_2 and sequence_name columns""",
                                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--in-metadata', dest = 'in_metadata', required=True, help='TSV from MAJORA')
     parser.add_argument('--out-metadata', dest = 'out_metadata', required=True, help='CSV to write out')
-    parser.add_argument('--accession-file', dest = 'accession_file', required=True, help='TSV of accessions')
+    parser.add_argument('--accession-file', dest = 'accession_file', required=False, help='TSV of accession')
+    parser.add_argument('--updated-date-file', dest = 'updated_date_file', required=False, help='CSV of date corrections')
     parser.add_argument('--log-file', dest = 'log_file', required=False, help='Log file')
 
     args = parser.parse_args()
 
     return args
 
-def add_sample_date(df):
-    sample_date = []
+def load_updated_dates(updated_date_file):
+    date_dict = {}
+    if updated_date_file:
+        with open(updated_date_file, 'r', newline = '') as dates_in:
+            reader = csv.DictReader(dates_in, delimiter=",", quotechar='\"', dialect = "unix")
+            for row in reader:
+                date_dict[row["central_sample_id"]] = row["sample_date"]
+    return date_dict
 
-    for i,row in df.iterrows():
-        if not pd.isnull(row['collection_date']) and row['collection_date'] != "None":
-            sample_date.append(row['collection_date'])
-        elif not pd.isnull(row['received_date']) and row['received_date'] != "None":
-            sample_date.append(row['received_date'])
-        else:
-            sample_date.append("")
+def add_sample_date(row, date_dict):
+    if row["central_sample_id"] in date_dict:
+        row["sample_date"] = date_dict[row["central_sample_id"]]
+        return
+    try:
+        date = datetime.strptime(row["collection_date"], '%Y-%m-%d').date()
+        row["sample_date"] = row["collection_date"]
+    except:
+        try:
+            date = datetime.strptime(row["received_date"], '%Y-%m-%d').date()
+            row["sample_date"] = row["received_date"]
+        except:
+            row["sample_date"] = ""
 
-    df['sample_date'] = sample_date
-    return df
+def add_pillar_2(row):
+    if row['collection_pillar'] == 2 or row['central_sample_id'][0:4] in ["ALDP", "CAMC", "MILK", "QEUH"]:
+        row["pillar_2"] = True
+    else:
+        row["pillar_2"] = False
 
-def add_pillar_2(df):
-    pillar_2 = []
+def add_sequence_name(row):
+    country = adm1a_to_country[row['adm1']]
+    id = row['central_sample_id']
+    year = str(row['sample_date']).split("-")[0]
+    name = country + "/" + id + "/" + year
 
-    for i,row in df.iterrows():
-        if row['collection_pillar'] == 2 or row['central_sample_id'][0:4] in ["ALDP", "CAMC", "MILK", "QEUH"]:
-            pillar_2.append(True)
-        else:
-            pillar_2.append(False)
+    row["sequence_name"] = name
 
-    df['pillar_2'] = pillar_2
-    return df
+def load_accession(accession_file, log_handle):
+    if not accession_file:
+        return {}
 
-def add_sequence_name(df):
-    adm1_to_country = {"UK-SCT": "Scotland",
-                       "UK-WLS": "Wales",
-                       "UK-ENG": "England",
-                       "UK-NIR": "Northern_Ireland"}
+    accession_dict = {}
 
-    sequence_name = []
+    with open(str(accession_file), 'r', newline = '') as acc_in:
+        reader = csv.DictReader(acc_in, delimiter="\t", quotechar='\"', dialect = "unix")
+        for row in reader:
+            central_sample_id = row["central_sample_id"]
+            run_name = row["run_name"]
+            gisaid_accession = row["gisaid.accession"]
 
-    for i,row in df.iterrows():
-        country = adm1_to_country[row['adm1']]
-        id = row['central_sample_id']
-        year = str(row['sample_date']).split("-")[0]
-        name = country + "/" + id + "/" + year
+            if central_sample_id in accession_dict:
+                if run_name in accession_dict[central_sample_id]:
+                    log_handle.write(f'duplicate central_sample_id * run_name in accession list: {central_sample_id} {run_name}\n')
+                    continue
+                accession_dict[central_sample_id][run_name] = gisaid_accession
+            else:
+                accession_dict[central_sample_id] = {run_name: gisaid_accession}
+    return accession_dict
 
-        sequence_name.append(name)
+def add_covv_accession_id(row, accession_dict):
+    acc = ""
+    if row["central_sample_id"] in accession_dict:
+        if row["run_name"] in accession_dict[row["central_sample_id"]]:
+            acc = accession_dict[row["central_sample_id"]][row["run_name"]]
 
-    df['sequence_name'] = sequence_name
-    return df
+    row["covv_accession_id"] = acc
 
-def load_accessions(accession_file, log_handle):
-    df_acc = pd.read_csv(accession_file, sep='\t')
-    accessions_dict = {}
-    for i,row in df_acc.iterrows():
-        central_sample_id = row["central_sample_id"]
-        run_name = row["run_name"]
-        gisaid_accession = row["gisaid.accession"]
+def date_string_to_epi_week(date_string):
+    """
+    parse a date string in YYYY-MM-DD format and return
+    cumulative epi week which is cumulative total epidemiological
+    weeks since 2019-12-22. Week beginning 2019-12-22 is week 0
+    """
+    try:
+        date = datetime.strptime(date_string, '%Y-%m-%d').date()
+    except:
+        return ""
+    # this is epi-week:
+    week = Week.fromdate(date)
+    if week.year < 2019 or (week.year == 2019 and week.week < 52):
+        return ""
+    elif week.year == 2019:
+        return("0")
+    else:
+        cum_epi_week = week.week + len(list(chain(*[[x for x in Year(y).iterweeks()] for y in range(2020, week.year)])))
+        return str(cum_epi_week)
 
-        if central_sample_id in accessions_dict:
-            if run_name in accessions_dict[central_sample_id]:
-                log_handle.write(f'duplicate central_sample_id * run_name in accessions list: {central_sample_id} {run_name}\n')
-                continue
-            accessions_dict[central_sample_id][run_name] = gisaid_accession
-        else:
-            accessions_dict[central_sample_id] = {run_name: gisaid_accession}
-    return accessions_dict
+def date_string_to_epi_day(date_string):
+    """
+    parse a date string in YYYY-MM-DD format and return
+    cumulative epi day which is cumulative total days since 2019-12-22
+    """
+    try:
+        date = datetime.strptime(date_string, '%Y-%m-%d').date()
+    except:
+        return ""
+    # this is epi-week week:
+    week = Week.fromdate(date)
+    # this is day 1 of epi-week 0:
+    day_one = datetime.strptime("2019-12-22", '%Y-%m-%d').date()
+    if week.year < 2019 or (week.year == 2019 and week.week < 52):
+        return ""
+    else:
+        cum_epi_day = (date - day_one).days + 1
+        return str(cum_epi_day)
 
-def add_covv_accession_id(df, accession_file, log_handle):
-    accessions_dict = load_accessions(accession_file, log_handle)
+def add_epi_week_and_day(row):
+    date_str = row["sample_date"]
+    epi_week = date_string_to_epi_week(date_str)
+    epi_day = date_string_to_epi_day(date_str)
 
-    covv_accession_id = []
-    for i,row in df.iterrows():
-        acc = ""
-        if row["central_sample_id"] in accessions_dict:
-            if row["run_name"] in accessions_dict[row["central_sample_id"]]:
-                acc = accessions_dict[row["central_sample_id"]][row["run_name"]]
+    row["edin_epi_week"] = epi_week
+    row["edin_epi_day"] = epi_day
 
-        covv_accession_id.append(acc)
-
-    df['covv_accession_id'] = covv_accession_id
-    return df
+def United_Kingdom_to_UK(row):
+    row["adm0"] = row["adm0"].replace("United Kingdom", "UK")
 
 def main():
     args = parse_args()
@@ -100,13 +149,31 @@ def main():
     else:
         log_handle = sys.stdout
 
-    df = pd.read_csv(args.in_metadata, sep = "\t")
-    df = add_sample_date(df)
-    df = add_pillar_2(df)
-    df = add_sequence_name(df)
-    df = add_covv_accession_id(df, args.accession_file, log_handle)
+    date_dict = load_updated_dates(args.updated_date_file)
+    accession_dict = load_accession(args.accession_file, log_handle)
+    new_columns = ["sample_date", "pillar_2", "sequence_name", "covv_accession_id", "edin_epi_week", "edin_epi_day"]
 
-    df.to_csv(args.out_metadata, index=False, sep = ",")
+    with open(args.in_metadata, 'r', newline = '') as csv_in, \
+         open(args.out_metadata, 'w', newline = '') as csv_out:
+
+        reader = csv.DictReader(csv_in, delimiter="\t", quotechar='\"', dialect = "unix")
+        writer = csv.DictWriter(csv_out, fieldnames = reader.fieldnames + new_columns, delimiter=",", quotechar='\"', quoting=csv.QUOTE_MINIMAL, dialect = "unix")
+        writer.writeheader()
+
+        for row in reader:
+            try:
+                add_sample_date(row, date_dict)
+                add_pillar_2(row)
+                add_sequence_name(row)
+                add_covv_accession_id(row, accession_dict)
+                add_epi_week_and_day(row)
+                United_Kingdom_to_UK(row)
+                writer.writerow(row)
+            except:
+                log_handle.write(f"Error updating metadata for row")
+                log_handle.write(str(row))
+                sys.exit("Could not update metadata for row, check metadata fields")
+
 
     log_handle.close()
 
--- modules/preprocess_cog_uk.nf
+++ modules/preprocess_cog_uk.nf
@@ -16,7 +16,7 @@ process uk_strip_header_digits {
     file uk_fasta
 
     output:
-    file "${uk_fasta.baseName}.updated.fasta"
+    file "${uk_fasta.baseName}.header_stripped.fasta"
 
     script:
     """
@@ -24,7 +24,7 @@ process uk_strip_header_digits {
     from Bio import SeqIO
 
     fasta_in = SeqIO.parse("${uk_fasta}", "fasta")
-    with open("${uk_fasta.baseName}.updated.fasta", 'w') as f:
+    with open("${uk_fasta.baseName}.header_stripped.fasta", 'w') as f:
         for record in fasta_in:
             ID = record.description.split("|")[0]
             f.write(">" + ID + "\\n")
@@ -36,14 +36,15 @@ process uk_strip_header_digits {
 process uk_add_columns_to_metadata {
     /**
     * Takes the MAJORA TSV of metadata and adds/updates columns for sample_date, pillar_2,
-    * sequence_name and covv_accession_id
-    * @input uk_metadata, uk_accessions
+    * sequence_name, covv_accession_id, edin_epi_week, edin_epi_day and adm0
+    * @input uk_metadata, uk_accessions, uk_updated_dates
     * @output uk_metadata_updated
     */
 
     input:
     file uk_metadata
     file uk_accessions
+    file uk_updated_dates
 
     output:
     file "${uk_metadata.baseName}.updated.csv"
@@ -53,49 +54,149 @@ process uk_add_columns_to_metadata {
     $project_dir/../bin/add_to_uk_metadata.py \
         --in-metadata ${uk_metadata} \
         --out-metadata ${uk_metadata.baseName}.updated.csv \
-        --accession-file ${uk_accessions}
+        --accession-file ${uk_accessions} \
+        --updated-date-file ${uk_updated_dates}
     """
 }
 
+process uk_filter_omitted_sequences {
+    /**
+    * Takes a FASTA and METADATA and excludes samples specified in an exclusion file
+    * sequence_name, covv_accession_id, edin_epi_week, edin_epi_day and adm0
+    * @input uk_fasta, uk_metadata, uk_omissions
+    * @output uk_fasta_updated, uk_metadata_updated
+    */
+    input:
+    file uk_fasta
+    file uk_metadata
 
-process uk_annotate_to_remove_duplicates {
+    output:
+    path "${uk_fasta.baseName}.omit_filtered.fa", emit: fasta
+    path "${uk_metadata.baseName}.omit_filtered.csv", emit: metadata
+
+    script:
+    if ( params.uk_omissions )
+        """
+        from Bio import SeqIO
+        import csv
+
+        alignment = SeqIO.index("${uk_fasta}", "fasta")
+
+        omissions = set()
+        with open("${params.uk_omissions}", "r") as f:
+            for line in f:
+                omissions.add(line.rstrip())
+
+        with open("${uk_metadata}", 'r', newline = '') as csv_in, \
+             open("${uk_metadata.baseName}.omit_filtered.csv", 'w', newline = '') as csv_out, \
+             open("${uk_fasta.baseName}.omit_filtered.fa", "w") as fasta_out:
+
+            reader = csv.DictReader(csv_in, delimiter=",", quotechar='\"', dialect = "unix")
+            writer = csv.DictWriter(csv_out, fieldnames = reader.fieldnames, delimiter=",", quotechar='\"', quoting=csv.QUOTE_MINIMAL, dialect = "unix")
+            writer.writeheader()
+
+            for row in reader:
+                if row["central_sample_id"] in omissions:
+                    continue
+
+                record = alignment[row["fasta_header"]]
+                writer.writerow(row)
+                fasta_out.write(">" + record.id + "\\n")
+                fasta_out.write(str(record.seq) + "\\n")
+        """
+    else
+        """
+        mv "${uk_fasta}" "${uk_fasta.baseName}.omit_filtered.fa"
+        mv "${uk_metadata}" "${uk_metadata.baseName}.omit_filtered.csv"
+        """
+}
+
+process uk_filter_on_sample_date {
+    /**
+    * If a time window (in days) is provided, excludes samples from FASTA and
+    * METADATA files which do not fall within X days of date
+    * @input uk_fasta, uk_metadata, time_window, date
+    * @output uk_fasta_update, uk_metadata_updated
+    */
 
     input:
     file uk_fasta
     file uk_metadata
 
     output:
-    file "${uk_metadata.baseName}.annotated.csv"
+    path "${uk_fasta.baseName}.date_filtered.fa", emit: fasta
+    path "${uk_metadata.baseName}.date_filtered.csv", emit: metadata
 
     script:
-    """
-    fastafunk annotate \
-        --in-fasta ${uk_fasta} \
-        --in-metadata ${uk_metadata} \
-        --out-metadata ${uk_metadata.baseName}.annotated.csv \
-        --index-column fasta_header
-    """
+    if ( params.time_window && params.date)
+        """
+        #!/usr/bin/env python3
+        import datetime
+        from Bio import SeqIO
+        import csv
+
+        indexed_fasta = SeqIO.index(str(input.fasta), "fasta")
+
+        window = datetime.timedelta(int(input.time_window))
+        todays_date = datetime.datetime.strptime(str(input.date), '%Y-%m-%d').date()
+
+        with open(str(input.metadata), 'r', newline = '') as csv_in, \
+            open("${uk_metadata.baseName}.date_filtered.csv", 'w', newline = '') as csv_out, \
+            open("${uk_fasta.baseName}.date_filtered.fa", "w") as fasta_out:
+
+            reader = csv.DictReader(csv_in, delimiter=",", quotechar='\"', dialect = "unix")
+            writer = csv.DictWriter(csv_out, fieldnames = reader.fieldnames, delimiter=",", quotechar='\"', quoting=csv.QUOTE_MINIMAL, dialect = "unix")
+            writer.writeheader()
+
+            for row in reader:
+                try:
+                    date = datetime.datetime.strptime(row["sample_date"], '%Y-%m-%d').date()
+                except:
+                    continue
+
+                if (todays_date - window) > date:
+                    continue
+
+                writer.writerow(row)
+
+                seq_rec = indexed_fasta[row["fasta_header"]]
+                fasta_out.write(">" + seq_rec.id + "\\n")
+                fasta_out.write(str(seq_rec.seq) + "\\n")
+        """
+    else
+        """
+        mv "${uk_fasta}" "${uk_fasta.baseName}.date_filtered.fa"
+        mv "${uk_metadata}" "${uk_metadata.baseName}.date_filtered.csv"
+        """
 }
 
-
-workflow reformat_fasta_and_metadata {
+workflow preprocess_cog_uk {
     take:
         uk_fasta
         uk_metadata
         uk_accessions
+        uk_updated_dates
     main:
         uk_strip_header_digits(uk_fasta)
-        uk_add_columns_to_metadata(uk_metadata, uk_accessions)
-        uk_annotate_to_remove_duplicates(uk_strip_header_digits.out, uk_add_columns_to_metadata.out)
+        uk_add_columns_to_metadata(uk_metadata, uk_accessions, uk_updated_dates)
+        uk_filter_omitted_sequences(uk_strip_header_digits.out, uk_add_columns_to_metadata.out)
+        uk_filter_on_sample_date(uk_filter_omitted_sequences.out.fasta, uk_filter_omitted_sequences.out.metadata)
     emit:
-        uk_strip_header_digits.out
-        uk_annotate_to_remove_duplicates.out
+        uk_filter_on_sample_date.out.fasta
+        uk_filter_on_sample_date.out.metadata
 }
 
 params.uk_fasta = file("test/matched.fa")
 params.uk_metadata = file("test/matched.tsv")
 params.uk_accessions = file("test/accessions.tsv")
+params.uk_updated_dates = ""
+params.uk_omissions = ""
+params.time_window = false
+params.date = false
 
 workflow {
-    reformat_fasta_and_metadata(params.uk_fasta, params.uk_metadata, params.uk_accessions)
+    preprocess_cog_uk(params.uk_fasta,
+                      params.uk_metadata,
+                      params.uk_accessions,
+                      params.uk_updated_dates)
 }
